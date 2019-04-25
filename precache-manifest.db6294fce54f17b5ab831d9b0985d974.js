8b83b20e9818411756cc59ddb683a57d8daa5d8f
Denis Jeong Plaster <djeong@mhccom.com>
2019-04-25 13:32:06 -0500

    Updates

---
+++ precache-manifest.db6294fce54f17b5ab831d9b0985d974.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "512c691ef9e6310fa7c68576acf8d2dd",
+    "url": "/denisplaster/wireframe-dashboard.git/index.html"
+  },
+  {
+    "revision": "3be4ef78625dc29c9352",
+    "url": "/denisplaster/wireframe-dashboard.git/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/denisplaster/wireframe-dashboard.git/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "3be4ef78625dc29c9352",
+    "url": "/denisplaster/wireframe-dashboard.git/static/js/main.ef1b2d26.chunk.js"
+  },
+  {
+    "revision": "f80df095db4f021fdf9d",
+    "url": "/denisplaster/wireframe-dashboard.git/static/js/runtime~main.647fca07.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/denisplaster/wireframe-dashboard.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
